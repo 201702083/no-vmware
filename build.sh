02360dc0cdd5c9d0e62529eca91cab5e80717a12
201702083 <201702083@o.cnu.ac.kr>
2022-03-15 15:39:35 +0900

    :construction: host / attacker 분리 실행

--- build.sh
+++ build.sh
@@ -1,8 +1,28 @@
 # !bin/bash
 
+CHECK_OS="`uname -s`" 
+if [[ "$CHECK_OS" = "Darwin"* ]]; then
+     # Do something under Mac OS X platform
+     THIS_OS="MAC"
+elif [[ "$CHECK_OS" = "Linux"* ]]; then
+     # Do something under GNU/Linux platform     
+     THIS_OS="LINUX" 
+elif [[ "$CHECK_OS" = "MINGW32"* ]]; then
+     # Do something under Windows NT platform     
+     THIS_OS="WIN" 
+elif [[ "$CHECK_OS" = "MINGW64"* ]]; then 
+     # Do something under Windows NT platform
+     THIS_OS="WIN" 
+elif [[ "$CHECK_OS" = "CYGWIN"* ]]; then
+     # Do something under Windows NT platform
+     THIS_OS="WIN"     
+fi
+
+echo "my os is $THIS_OS"
+
 if [ "$1" == "host" ] ; then
 
-  docker compose up -d --build
+  docker compose up -d --build host
 
   echo "Connected to seed ubuntu : host"
 
@@ -10,7 +30,7 @@ if [ "$1" == "host" ] ; then
 
 elif [ "$1" == "attacker" ] ; then
 
-  docker compose up -d --build
+  docker compose up -d --build attacker
 
   echo "Connected to seed ubuntu : attacker"
 
