4d252b96f596c298a566014fc3c0307d78a0c4bf
201702083 <201702083@o.cnu.ac.kr>
2022-05-17 09:44:58 +0900

    :green_heart: Add more sequence

--- kaliexercise/kali/victim/setup.sh
+++ kaliexercise/kali/victim/setup.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
+# setup for dvwa
 unzip /kali/victim/DVWA-master.zip
 cp -r /kali/victim/DVWA-master /var/www/html/dvwa
 chmod -R 755 /var/www/html/dvwa
 chown -R www-data:www-data /var/www/html/dvwa
+
+# dvwa config
+
+cp /var/www/html/dvwa/config/config.inc.php.dist /var/www/html/dvwa/config/config.inc.php
+
+apt-get update && apt-get install default-mysql-server php-mysqli -y
+
+sudo service mariadb start
+mysql -u root -p{} -e "create database dvwa; create user 'dvwa'@'127.0.0.1' identified by 'p@ssw0rd'; grant all on dvwa.* to 'dvwa'@'127.0.0.1'; flush privileges;"
+
+sudo service apache2 start
+
+echo "extension_dir=/usr/lib/php/20210902" >> /etc/php/8.1/apache2/php.ini
