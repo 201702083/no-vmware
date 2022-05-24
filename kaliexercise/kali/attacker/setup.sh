dd8fc75f1c42f2c081e55a0cc57f9db652a94b6f
201702083 <201702083@o.cnu.ac.kr>
2022-05-24 21:00:37 +0900

    :rocket: del

--- kaliexercise/kali/attacker/setup.sh
+++
@@ -1,5 +0,0 @@
-!/bin/bash
-
-cp -r DVWA-master /var/www/html/dvwa
-chmod -R 755 /var/www/html/dvwa
-chown -R www-data:www-data /var/www/html/dvwa
--- reset.sh
+++
@@ -1,3 +0,0 @@
-docker compose down
-docker rm -v -f attacker
-docker rm -v -f host-10.0.2.5
